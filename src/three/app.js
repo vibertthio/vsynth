229d21728fa8ed1220b6076739f86099ed4bc68c
Vibert Thio <vibertthio@gmail.com>
2017-12-28 18:07:01 +0800

    :hammer: keep the entry file

--- src/three/app.js
+++ src/three/app.js
@@ -2,14 +2,14 @@
 //
 // box();
 
-// import particles from './particles/';
-//
-// particles();
+import particles from './particles/';
+
+particles();
 
 
-// HMR Hot Module Replacement
-// if (module.hot) {
-//   module.hot.accept(() => {
-//     window.location.reload(true);
-//   });
-// }
+// HMR: Hot Module Replacement
+if (module.hot) {
+  module.hot.accept(() => {
+    window.location.reload(true);
+  });
+}
